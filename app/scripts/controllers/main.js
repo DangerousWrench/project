c36570ea876d1cc25eec23f535744386897c6700
Lauren <lpappone@gmail.com>
2014-08-29 12:05:43 -0700

    add search results controller file

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,28 @@
  * # MainCtrl
  * Controller of the dangerousWrenchApp
  */
-angular.module('dangerousWrenchApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+angular.module('dangerousWrenchApp', [])
+  .controller('MainCtrl', function ($scope, 'PopularImages') {
+    $scope.getPopImages = PopularImages.getPopImages; //used on ng-click on image 
   });
+
+
+  //service (factory?) to get current popular images if someone clicks on one of those
+  .service('PopularImages', function($http) {
+    this.getPopImages = function(<<ng-click target?>>)
+      return $http({
+        method: 'GET',
+        url: 'api/????'
+      })
+      .then(function(response) {
+        //go to view page with popular images?
+      })
+        .catch(function(error) {
+          //error handler
+        })
+  });
+
+
+
+
+  //service for auth/login?
---
+++ app/scripts/controllers/searchresults.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dangerousWrenchApp.controller:SearchResultsCtrl
+ * @description
+ * # SearchResultsCtrl
+ * Controller of the dangerousWrenchApp
+ */
+angular.module('dangerousWrenchApp', [])
+  .controller('SearchResultsCtrl', function ($scope) {
+
+  });
