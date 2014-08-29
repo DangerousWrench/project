0affc6e765a0ac841bb54a4cd0db79ceacaa2e07
Lauren <lpappone@gmail.com>
2014-08-29 13:22:02 -0700

    remove search service

--- client/app/scripts/services/services.js
+++ client/app/scripts/services/services.js
@@ -2,16 +2,4 @@
 
 angular.module('dangerousWrenchServices', [])
 
-  .factory('Main', function ($http) {
-    
-    var search = function(url) {
-      method: 'GET',
-      url: '/api/ ---path to images database---'
-      data: 
-    }
-
-    return {
-      search: search
-    };
-    
-  })
+  
