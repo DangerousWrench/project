0b3712564ef9ad1c45dcf160a9c68edb4ccfc580
Mario <mpontice@gmail.com>
2014-08-29 16:38:05 -0700

    add GrapheneDB information to server/routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,9 @@
 //establish a connection to the database
+var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase(
+    process.env['GRAPHENEDB_URL'] ||
+    'http://localhost:7474'
+);
 
 module.exports = function(app){
   app.get('/api/painting', function(req, res){
