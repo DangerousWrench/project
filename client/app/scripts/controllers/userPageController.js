153aa63f7c393f5842fa646d62669d2fe12c4c47
Austin Connelly <ajjconnelly@gmail.com>
2014-09-02 10:44:58 -0700

    (feature) add user page services, view and controller

---
+++ client/app/scripts/controllers/userPageController.js
@@ -0,0 +1,47 @@
+use 'strict';
+
+angular.module('dangerousWrenchApp')
+  .controller('UserController', function ($scope, KeywordSearch, userServices) {
+    
+    //included searchterms in case a search bar is included in the user page
+    $scope.searchterms;
+    $scope.search = KeywordSearch.search;
+
+
+
+
+
+    //Im not quite sure how the recommendation engine will work, but 
+    // my small mind tells me it will be something like the following
+    // 
+    //generateUserLikes is the factory function that queries 
+    //for a specific users 'liked' art
+    $scope.displayUserLikes = userServices.generateUserLikes;
+    $scope.displayUserLikes()
+      .then(function(data) {
+        $scope.userLikesResults = data.data;
+        console.log($scope.userLikesResults);
+        console.log('Retrieved users likes!');
+      })
+      .catch(function(){
+        console.log('Failed to find users likes :|')
+      })
+
+
+    //generateUserRecommendations is the factory function that queries
+    //for a specific users recommended pieces
+    $scope.displayUserRecommendations = userServices.generateUserRecommendations;
+    $scope.displayUserRecommendations(); 
+      .then(function(data) {
+        $scope.userRecommendationsResults = data.data;
+        console.log($scope.userRecommendationsResults);
+        console.log('Generated user recommendations!');
+      })
+      .catch(function(){
+        console.log('Failed to generate user recommendations :|')
+      })      
+
+
+      
+});
+
