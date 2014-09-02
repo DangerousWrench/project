c3d6f470728e526f7a12f798c4fa9a77d18a4ccd
Mario <mpontice@gmail.com>
2014-09-01 21:39:13 -0700

    revise 'GenerateArtInfo' factory and 'ArtCtrl' to use promises, remove 'LikeArt' factory, move 'like' functionality to view

--- client/app/scripts/controllers/art.js
+++ client/app/scripts/controllers/art.js
@@ -1,8 +1,15 @@
 angular.module('dangerousWrenchApp')
-	.controller('ArtCtrl', function($scope, LikeArt, GenerateArtInfo) {
+	.controller('ArtCtrl', function($scope, GenerateArtInfo) {
 		// assign $scope the generate function from the GenerateArtInfo service
 		$scope.generate = GenerateArtInfo.generate;
 		// then invoke it on the next line, so that $scope.results has data to pass along to the view
-		$scope.generate();
-		$scope.like = LikeArt.like;
+		$scope.generate()
+			.then(function(data) {
+				$scope.results = data.data;
+				console.log($scope.results);
+				console.log('Successfully generated info!');
+			})
+			.catch(function() {
+				console.log('Failed to generate info!');
+			})
 	});
--- client/app/scripts/services/artServices.js
+++ client/app/scripts/services/artServices.js
@@ -1,33 +1,10 @@
 angular.module('dangerousWrenchApp')
-	.factory('LikeArt', function($http) {
-		this.like = function() {
-			return $http({
-				method: 'POST',
-				url: '', // not sure about this
-				data: {} // insert query here; would create an edge between the art and user nodes
-			})
-			.then(function() {
-				console.log('Successfully liked!');
-			})
-			.catch(function() {
-				console.log('Unable to like piece!');
-			})
-		};
-	})
 	.factory('GenerateArtInfo', function($http) {
 		this.generate = function() {
 			return $http({
 				method: 'POST',
 				url: '', // not sure about this
-				data: {} // insert the query here; would return all nodes connected to the piece
-			})
-			.then(function(data) {
-				this.results = data.data // may change based on DB formatting
-				console.log(this.results);
-				console.log('Successfully generated info!');
-			})
-			.catch(function(error) {
-				console.log('Failed to generate info!');
+				data: {} // insert the query here; will return all nodes connected to the piece
 			})
 		};
 	});
