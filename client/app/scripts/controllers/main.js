f1c75791e10db47eb87bbb5b4ca769c77f96ec67
Lauren <lpappone@gmail.com>
2014-09-06 15:05:30 -0700

    clean up search results controller and service

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,45 +8,29 @@
  * Controller of the dangerousWrenchApp
  */
 angular.module('dangerousWrenchApp')
+
   .controller('MainCtrl', function ($scope, KeywordSearch) {
     $scope.searchterms;
-    $scope.search = KeywordSearch.search; 
-    $scope.displayResults = KeywordSearch.displayResults;
-
-    $scope.search = function() {
-      $scope.displayResults($scope.searchterms);
-    //   $scope.search.results = KeywordSearch.search($scope.searchterms)
-    //     .then(function (response) {
-    //       $scope.artData = response.data;
-    //       $scope.displayResults(response);
-    //       console.log("scope.artdata main= ", $scope.artData);
-    //     }, function (error) {
-    //       console.log(error);
-    //     })
+    $scope.displayResults = function() {
+      KeywordSearch.displayResults($scope.searchterms);
     };
-
-    
-})
+  })
 
   .controller('SearchResultsCtrl', function ($scope, $rootScope, KeywordSearch) {
     $scope.searchterms;
     $scope.artData = {};
     $scope.search = function() {
-      $scope.search.results = KeywordSearch.search($scope.searchterms)
+      KeywordSearch.search($scope.searchterms)
         .then(function (response) {
           $scope.artData = response.data;
-          // $scope.displayResults(response);
-          console.log("scope.artdata searchresults = ", $scope.artData);
         }, function (error) {
           console.log(error);
         })
     };
     $rootScope.$on('redirected', function (event, data) {
-      console.log('in scope.on', data.data)
       $scope.searchterms = data.data;
       $scope.search();
     });
-    //$scope.select = SelectPiece.getimage;
 })
 
 
