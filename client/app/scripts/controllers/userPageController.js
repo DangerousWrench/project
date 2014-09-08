158d02c9793db0a2af5d9359f3b0a3ef683ca4ae
Lauren <lpappone@gmail.com>
2014-09-08 11:29:52 -0700

    userpage controller and factor

--- client/app/scripts/controllers/userPageController.js
+++ client/app/scripts/controllers/userPageController.js
@@ -2,14 +2,26 @@
 
 angular.module('dangerousWrenchApp')
   .controller('UserController', function ($scope, KeywordSearch, userServices) {
+<<<<<<< HEAD
     
     //Im not quite sure how the recommendation engine will work, but 
     // my small mind tells me it will be something like the following
     // 
+=======
+
+    $scope.searchterms;
+    $scope.displayResults = function() {
+      KeywordSearch.displayResults($scope.searchterms);
+    };
+
+    $scope.username = $location.search().q;
+  
+>>>>>>> userpage controller and factor
     //generateUserLikes is the factory function that queries 
     //for a specific users 'liked' art
     $scope.displayUserLikes = userServices.generateUserLikes;
-    $scope.displayUserLikes()
+
+    $scope.displayUserLikes($scope.username)
       .then(function(data) {
         $scope.userLikesResults = data.data;
         console.log($scope.userLikesResults);
@@ -23,7 +35,7 @@ angular.module('dangerousWrenchApp')
     //generateUserRecommendations is the factory function that queries
     //for a specific users recommended pieces
     $scope.displayUserRecommendations = userServices.generateUserRecommendations;
-    $scope.displayUserRecommendations()
+    $scope.displayUserRecommendations($scope.username)
       .then(function(data) {
         $scope.userRecommendationsResults = data.data;
         console.log($scope.userRecommendationsResults);
@@ -31,9 +43,6 @@ angular.module('dangerousWrenchApp')
       })
       .catch(function(){
         console.log('Failed to generate user recommendations :|')
-      })      
-
-
-      
+      })           
 });
 
